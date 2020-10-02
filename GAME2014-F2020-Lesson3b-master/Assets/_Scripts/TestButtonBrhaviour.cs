bbfd33886e8a47b469ec45a9bdaa0a3e8e9d213e
Blair-White <blairjwhite@hotmail.com>
2020-10-01 21:04:42 -0400

    Lab2 First Push
    Change label anchored position according to phone orientation

---
+++ GAME2014-F2020-Lesson3b-master/Assets/_Scripts/TestButtonBrhaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestButtonBrhaviour : MonoBehaviour
+{
+    //public TMP_Text SceneLabel;
+    public TMP_Text LivesLabel, ScoreLabel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("Test Button Pressed");
+        LivesLabel.rectTransform.anchoredPosition = new Vector2(438.0f, -207.0f);
+        ScoreLabel.rectTransform.anchoredPosition = new Vector2(-350.0f, -207.0f);
+    }
+
+}
